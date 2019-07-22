e86355423190a79035413dcb29d2ddfac8b14096
jerryjiao <jerry201382@gmail.com>
2019-07-22 16:43:46 +0800

    feat(all): add base functions

---
+++ index.js
@@ -0,0 +1,76 @@
+(function (global, factory) {
+  typeof exports == 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.E = {})))
+})(this, (function (exports) { 'use strict';
+  /**
+   * Always Return False
+   * 
+   * @returns
+   */
+  const F = function () {return false}
+
+  /**
+   *Always Return True
+   *
+   * @returns
+   */
+  const T = function() {return true}
+
+  /**
+   *
+   *
+   * @param {*} fns
+   */
+  const pipe = (...fns) => x => fns.reduce((acc, fn) => fn(acc),x)
+
+  /**
+   * trace pipe to console
+   *
+   * @param {*} msg
+   */
+  const trace = msg => x => (console.log(msg,x), x)
+
+  /**
+   *
+   *
+   * @param {*} fn
+   */
+  const map = fn => arr => arr.map(fn)
+
+  /**
+   *
+   *
+   * @param {*} fn
+   */
+  const filter = fn => arr => arr.filter(fn)
+
+  /**
+   *
+   *
+   * @param {*} fn
+   */
+  const foreach = fn => arr => arr.forEach(fn)
+
+  /**
+   *
+   *
+   * @param {*} fn
+   */
+  const concat = fn => arr => arr.concat(fn)
+
+  /**
+   *
+   *
+   * @param {*} x
+   */
+  const either = x => y => x||y
+  
+  /**
+   *
+   *
+   * @param {*} x
+   */
+  const both = x => y => x&&y
+
+}))
